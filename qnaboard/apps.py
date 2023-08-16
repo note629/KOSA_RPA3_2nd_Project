ad278f5f1a635e709fffc67532d4f3032a329388
note629 <note629@gmail.com>
2023-08-16 10:35:51 +0900

    feat: main branch upload

---
+++ qnaboard/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class QnaboardConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "qnaboard"
---
+++ user/models.py
@@ -0,0 +1,2 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
