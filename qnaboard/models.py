179ec6a2a1c657d42364e38e6f5ec63a2cea77bb
2hayoen <leehayoen0518@gmail.com>
2023-08-16 15:44:35 +0900

    feat : QnA Board model 작성

--- qnaboard/models.py
+++ qnaboard/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+# 게시글
+class Post(models.Model):
+    user = models.ForeignKey("users.User", verbose_name="작성자", on_delete=models.CASCADE)
+    p_content = models.TextField("내용")
+    p_date = models.DateTimeField("작성일", auto_now_add=True)
+
+    def __str__(self):
+        return self.p_title
+
+
+# 댓글
+class Comment(models.Model):
+    user = models.ForeignKey("users.User", verbose_name="작성자", on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    c_content = models.TextField("내용")
+    c_date = models.DateTimeField("작성일", auto_now_add=True)
+
+    def __str__(self):
+        return self.c_content
