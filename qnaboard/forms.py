a514036d433d5322c0bf43e674f6bed6649b7d02
2hayoen <leehayoen0518@gmail.com>
2023-08-17 17:23:16 +0900

    feat : QnA 글작성 forms.py, html 작성완료, +QnA List CSS

---
+++ qnaboard/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from qnaboard.models import Post
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["p_title", "user", "p_content"]
+        # fields = ["p_title", "p_content"]
+
+        # labels = {
+        #     "p_title": "제목",
+        #     "user": "작성자",
+        #     "p_content": "내용",
+        # }
+        labels = {
+            "p_title": "제목",
+            "p_content": "내용",
+        }
+
+        # 박스
+        widgets = {
+            "p_title": forms.TextInput(
+                attrs={
+                    "class": "form-control w-50",  ## CSS
+                    "placeholder": "질문 사항이나 인증내용의 제목을 입력해 주세요",  ## 힌트
+                }
+            ),
+            # "user": forms.TextInput(
+            #     attrs={"class": "form-control w-25", "placeholder": "작성자"}
+            # ),
+            "p_content": forms.Textarea(
+                attrs={
+                    "class": "form-control w-75",
+                    "placeholder": "자세한 질문 사항이나 인증 내용을 입력해주세요",
+                }
+            ),
+        }
