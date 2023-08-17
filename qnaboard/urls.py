a6c474431f8755639959661d856c38c930fdf7e4
2hayoen <leehayoen0518@gmail.com>
2023-08-17 17:13:13 +0900

    feat : QnA Board-list, create Path 경로 추가

--- qnaboard/urls.py
+++ qnaboard/urls.py
@@ -1,23 +1,12 @@
-"""RecyclableProject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 
+from qnaboard import views
+
 app_name = "qnaboard"
 
+## 현재 URL => http://127.0.0.1:8000/qnaboard
 urlpatterns = [
-
+    path("list/", views.p_list, name="list"),
+    path("create/", views.p_create, name="create"),
 ]
