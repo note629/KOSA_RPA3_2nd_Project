ca9e1e872ea2183acd51a9575b1011b92adef24e
2hayoen <leehayoen0518@gmail.com>
2023-08-17 17:28:40 +0900

    feat : 추후 재수정

--- RecyclableProject/urls.py
+++ RecyclableProject/urls.py
@@ -18,8 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="home.html"),
-         name='home'),
+    path("", TemplateView.as_view(template_name="home.html"), name="home"),
     path("admin/", admin.site.urls),
     path("qnaboard/", include("qnaboard.urls")),
     path("users/", include("users.urls")),
