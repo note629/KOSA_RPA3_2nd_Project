436bff987eb6a1b4fc376afb7203761d45f90d9e
2hayoen <leehayoen0518@gmail.com>
2023-08-18 14:44:21 +0900

    feat : QnA Board delect(qb_delete)삭제 기능 구현

--- qnaboard/urls.py
+++ qnaboard/urls.py
@@ -8,7 +8,8 @@ app_name = "qnaboard"
 ## 현재 URL => http://127.0.0.1:8000/qnaboard
 urlpatterns = [
     path("list/", views.qb_list, name="list"),
-    path("<int:post_id>/", views.qb_read, name="read"),
+    path("read/<int:post_id>/", views.qb_read, name="read"),
     path("create/", views.qb_create, name="create"),
     path("update/<int:post_id>/", views.qb_update, name="update"),
+    path("delete/<int:post_id>/", views.qb_delete, name="delete"),
 ]
--- qnaboard/views.py
+++ qnaboard/views.py
@@ -1,25 +1,26 @@
-from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
 from requests import post
 
 from qnaboard.forms import PostForm
 from qnaboard.models import Post
 
 
-# 게시글 목록 조회
+# QnA 게시글 목록 조회
 def qb_list(request):
     posts = Post.objects.all().order_by("-qb_date")
     context = {"posts": posts}
     return render(request, "qnaboard/qna_list.html", context)
 
 
-# 게시글 상세 조회
+# QnA 게시글 상세 조회
 def qb_read(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {"post": post}
     return render(request, "qnaboard/qna_read.html", context)
 
 
-# 게시글 작성
+# QnA 게시글 작성
 def qb_create(request):
     if request.method == "POST":
         post_form = PostForm(request.POST)
@@ -35,7 +36,7 @@ def qb_create(request):
     return render(request, "qnaboard/qna_create.html", {"post_form": post_form})
 
 
-# 게시글 수정
+# QnA 게시글 수정
 def qb_update(request, post_id):
     post = Post.objects.get(id=post_id)
 
@@ -50,3 +51,14 @@ def qb_update(request, post_id):
     else:
         updateForm = PostForm(instance=post)
         return render(request, "qnaboard/qna_update.html", {"updateForm": updateForm})
+
+
+# QnA 게시글 삭제
+def qb_delete(request, post_id):
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return redirect("qnaboard:list")
+
+
+#     except Post.DoesNotExist:
+#         return HttpResponse("해당 게시물을 찾을 수 없습니다.")
