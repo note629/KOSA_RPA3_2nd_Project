82bbe38090a5fd73d789daeabd13c96721549806
2hayoen <leehayoen0518@gmail.com>
2023-08-18 17:25:02 +0900

    fix : 로그인 여부에 대한 QnA Board 글 작성, 수정, 삭제 권한 제한 기능 구현

--- qnaboard/forms.py
+++ qnaboard/forms.py
@@ -5,30 +5,21 @@ from qnaboard.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["qb_title", "user", "qb_content"]
-        # fields = ["qb_title", "qb_content"]
+        fields = ["qb_title", "qb_content"]
 
-        # labels = {
-        #     "p_title": "제목",
-        #     "user": "작성자",
-        #     "p_content": "내용",
-        # }
         labels = {
             "qb_title": "제목",
             "qb_content": "내용",
         }
 
-        # 박스
         widgets = {
             "qb_title": forms.TextInput(
                 attrs={
-                    "class": "form-control w-50",  ## CSS
-                    "placeholder": "질문 사항이나 인증내용의 제목을 입력해 주세요",  ## 힌트
+                    "class": "form-control w-50",
+                    "placeholder": "질문 사항이나 인증내용의 제목을 입력해 주세요",
+                    "style": "margin-bottom : 10px;",
                 }
             ),
-            # "user": forms.TextInput(
-            #     attrs={"class": "form-control w-25", "placeholder": "작성자"}
-            # ),
             "qb_content": forms.Textarea(
                 attrs={
                     "class": "form-control w-75",
@@ -36,3 +27,15 @@ class PostForm(forms.ModelForm):
                 }
             ),
         }
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop("user", None)  # 사용자 정보를 받기 위한 변수
+        super().__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+        if self.user:
+            instance.user = self.user  # 로그인한 사용자 정보 설정
+        if commit:
+            instance.save()
+        return instance
--- qnaboard/views.py
+++ qnaboard/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect, get_object_or_404
-from requests import post
+from django.core.exceptions import PermissionDenied
+
+from django.shortcuts import render, redirect
 
 from qnaboard.forms import PostForm
 from qnaboard.models import Post
@@ -22,18 +22,21 @@ def qb_read(request, post_id):
 
 # QnA 게시글 작성
 def qb_create(request):
-    if request.method == "POST":
-        post_form = PostForm(request.POST)
+    if not request.user.is_authenticated:
+        return redirect("qnaboard:list")
 
-        if post_form.is_valid():
-            new_post = post_form.save(commit=False)
-            new_post.save()
-            return redirect("qnaboard:list")
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            post_form = PostForm(request.POST, user=request.user)
 
-    else:
-        post_form = PostForm()
+            if post_form.is_valid():
+                post_form.save()
+                return redirect("qnaboard:list")
 
-    return render(request, "qnaboard/qna_create.html", {"post_form": post_form})
+        else:
+            post_form = PostForm(user=request.user)
+
+        return render(request, "qnaboard/qna_create.html", {"post_form": post_form})
 
 
 # QnA 게시글 수정
@@ -41,21 +44,24 @@ def qb_update(request, post_id):
     post = Post.objects.get(id=post_id)
 
     if request.method == "POST":
-        post.qb_title = request.POST["qb_title"]
-        post.qb_content = request.POST["qb_content"]
+        post_form = PostForm(request.POST, instance=post, user=request.user)
 
-        post.save()
-        # return redirect("qnaboard:list")  # 리스트 페이지로 이동
-        return redirect("qnaboard:read", post_id=post.id)  # 수정된 게시물의 상세 페이지로 이동
+        if post_form.is_valid():
+            post_form.save()
+            return redirect("qnaboard:read", post_id=post.id)
 
     else:
-        updateForm = PostForm(instance=post)
-        return render(request, "qnaboard/qna_update.html", {"updateForm": updateForm})
+        post_form = PostForm(instance=post, user=request.user)
+
+    return render(request, "qnaboard/qna_update.html", {"updateForm": post_form})
 
 
 # QnA 게시글 삭제
 def qb_delete(request, post_id):
     post = Post.objects.get(id=post_id)
+    if request.user != post.user:  # 작성자만 삭제 가능
+        raise PermissionDenied  # 권한 없음
+
     post.delete()
     return redirect("qnaboard:list")
 
