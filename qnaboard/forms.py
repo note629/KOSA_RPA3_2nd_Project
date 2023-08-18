3da0374035e30fee8025fd92cf92321cc330f811
2hayoen <leehayoen0518@gmail.com>
2023-08-18 09:54:43 +0900

    fix : 모델 조회수 추가 + 변수명 p_ 에서 qb_로 변경함

--- qnaboard/forms.py
+++ qnaboard/forms.py
@@ -5,8 +5,8 @@ from qnaboard.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["p_title", "user", "p_content"]
-        # fields = ["p_title", "p_content"]
+        # fields = ["p_title", "user", "p_content"]
+        fields = ["qb_title", "qb_content"]
 
         # labels = {
         #     "p_title": "제목",
@@ -14,13 +14,13 @@ class PostForm(forms.ModelForm):
         #     "p_content": "내용",
         # }
         labels = {
-            "p_title": "제목",
-            "p_content": "내용",
+            "qb_title": "제목",
+            "qb_content": "내용",
         }
 
         # 박스
         widgets = {
-            "p_title": forms.TextInput(
+            "qb_title": forms.TextInput(
                 attrs={
                     "class": "form-control w-50",  ## CSS
                     "placeholder": "질문 사항이나 인증내용의 제목을 입력해 주세요",  ## 힌트
@@ -29,7 +29,7 @@ class PostForm(forms.ModelForm):
             # "user": forms.TextInput(
             #     attrs={"class": "form-control w-25", "placeholder": "작성자"}
             # ),
-            "p_content": forms.Textarea(
+            "qb_content": forms.Textarea(
                 attrs={
                     "class": "form-control w-75",
                     "placeholder": "자세한 질문 사항이나 인증 내용을 입력해주세요",
