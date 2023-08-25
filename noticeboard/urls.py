43e005e86d91e63acf8adae6ebc1215798c13e36
2hayoen <leehayoen0518@gmail.com>
2023-08-25 12:17:31 +0900

    feat : "noticeboard" notice CRUD(작성,수정,삭제 기능) 구현

--- noticeboard/urls.py
+++ noticeboard/urls.py
@@ -9,6 +9,9 @@ app_name = "noticeboard"
 
 ## 현재 URL => http://127.0.0.1:8000/noticeboard
 urlpatterns = [
-    path("notices/", views.nb_list, name="notice_list"),
-    path("notices/<int:notice_id>/", views.nb_read, name="notice_read"),
+    path("list/", views.nb_list, name="list"),
+    path("read/<int:notice_id>/", views.nb_read, name="read"),
+    path("create/", views.nb_create, name="create"),
+    path("update/<int:notice_id>/", views.nb_update, name="update"),
+    path("delete/<int:notice_id>/", views.nb_delete, name="delete"),
 ]
